9be416a2f2b2ffd397e3b12513e77a0a008ca572
olexander05 <olexanderzoz@gmail.com>
2025-03-25 18:46:03 +0100

    39.HTML, CSS і основи мови розмітки

--- notes_app/notes/views.py
+++ notes_app/notes/views.py
@@ -1,4 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello from Notes app.")
+
+    notes = [
+        {
+            'title': 'Note 1',
+            'content': 'This is the text of my first note.',
+            'created_at': datetime(2025, 3, 25, 12, 0)
+        },
+        {
+            'title': 'Note 2',
+            'content': 'This is the text of the second note, a bit longer.',
+            'created_at': datetime(2025, 3, 24, 15, 30)
+        },
+        {
+            'title': 'Note 3',
+            'content': 'The third note for testing.',
+            'created_at': datetime(2025, 3, 23, 9, 15)
+        },
+    ]
+    return render(request, 'notes/index.html', {'notes': notes})
